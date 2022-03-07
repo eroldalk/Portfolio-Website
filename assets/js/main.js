13507aa5dc2786d54dcefe5b74331f882d05860d
eroldalk <eroldalg@gmail.com>
2022-03-07 18:39:07 +0300

    testimonial page created

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,7 +106,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
@@ -120,7 +120,22 @@ let swiper = new Swiper('.portfolio__container', {
     },
 });
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints:{
+        568:{
+            slidesPerView: 2,
+        }
+    }
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
